24cce13ac343f08b1a6e57ea9240d34517214d4d
Olga Koplik <olga9194196@mail.ru>
2019-08-05 07:38:53 +0200

    Updates

---
+++ precache-manifest.ffca551b5c36eb2d9053259baf681248.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3065ca4935e1bd05e299e80485efdc25",
+    "url": "/OlgaKoplik/reactjs-portfolio/index.html"
+  },
+  {
+    "revision": "00e24de7ceda02666b31",
+    "url": "/OlgaKoplik/reactjs-portfolio/static/css/main.857ff844.chunk.css"
+  },
+  {
+    "revision": "8bf4b57117591159cdbe",
+    "url": "/OlgaKoplik/reactjs-portfolio/static/js/2.6ce00ae9.chunk.js"
+  },
+  {
+    "revision": "00e24de7ceda02666b31",
+    "url": "/OlgaKoplik/reactjs-portfolio/static/js/main.c76e3d15.chunk.js"
+  },
+  {
+    "revision": "ebdb97a7c9fcac68db31",
+    "url": "/OlgaKoplik/reactjs-portfolio/static/js/runtime~main.339d1ed3.js"
+  },
+  {
+    "revision": "d3dcd455259aa8af3cf813c3e55a4300",
+    "url": "/OlgaKoplik/reactjs-portfolio/static/media/Me.d3dcd455.jpg"
+  },
+  {
+    "revision": "4a88cd73e9713629deb08a55d57fafa6",
+    "url": "/OlgaKoplik/reactjs-portfolio/static/media/golf.4a88cd73.jpg"
+  }
+]);
